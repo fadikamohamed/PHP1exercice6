e5fe65d1e08995813ed54c1e861b62fc61108fa1
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:43:48 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+      $name = 'mohamed';
+      echo 'bonjour ' . $name . ' comment vas tu ?';
+
+     ?>
+
+  </body>
+</html>
